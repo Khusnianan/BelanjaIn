091593e8dd45e3343129d5872b7fb4f2ba84ee34
Khusnianan <muhammadkhusni96@wastukancana.ac.id>
2025-04-23 08:53:48 +0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,42 +1,15 @@
 import streamlit as st
-from db import get_connection
-from utils import hash_password, check_password
+import pandas as pd
+from sqlalchemy import create_engine
 
-def login_form():
-    st.subheader("Login")
-    email = st.text_input("Email", key="login_email")
-    password = st.text_input("Password", type="password", key="login_password")
+DATABASE_URL = "postgresql://postgres:iguefKwACKrTVamieBDyZxcjTKNDVcEG@maglev.proxy.rlwy.net:40486/railway"
+engine = create_engine(DATABASE_URL)
 
-    if st.button("Login"):
-        conn = get_connection()
-        cur = conn.cursor()
-        cur.execute("SELECT id, password FROM users WHERE email=%s", (email,))
-        result = cur.fetchone()
-        conn.close()
+def register_user(username, password):
+    with engine.connect() as connection:
+        connection.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
 
-        if result and check_password(password, result[1].encode()):
-            st.success("Login berhasil!")
-            st.session_state["logged_in"] = True
-            st.session_state["user_id"] = result[0]
-            st.session_state["page"] = "dashboard"
-            st.rerun()  # Refresh halaman
-        else:
-            st.error("Email atau password salah.")
-
-def register_form():
-    st.subheader("Daftar Akun Baru")
-    email = st.text_input("Email", key="register_email")
-    password = st.text_input("Password", type="password", key="register_password")
-
-    if st.button("Register"):
-        conn = get_connection()
-        cur = conn.cursor()
-        try:
-            cur.execute("INSERT INTO users (email, password) VALUES (%s, %s)", (email, hash_password(password)))
-            conn.commit()
-            st.success("Registrasi berhasil! Silakan login.")
-            st.session_state["page"] = "login"
-        except:
-            st.error("Email sudah digunakan.")
-        finally:
-            conn.close()
+def authenticate_user(username, password):
+    with engine.connect() as connection:
+        result = connection.execute("SELECT * FROM users WHERE username = %s AND password = %s", (username, password))
+        return result.fetchone() is not None
